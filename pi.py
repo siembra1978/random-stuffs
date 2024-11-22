f5abeb918d9056df7c393761966690658010bd65
siembra <77788776+siembra1978@users.noreply.github.com>
2024-11-22 16:11:53 -0500

    k

--- pi.py
+++ pi.py
@@ -5,8 +5,13 @@ def approximate_pi(n):
     f = n * sine(math.radians(180/n))
     return f
 
-#n = 10*10**307
-n=1
+n = 10*10**307
+#n=1
+
+'''
 while True:
     print(f'pi ~= {approximate_pi(n):.75f}')
     n+=1
+'''
+print(f'pi ~= {approximate_pi(n):.75f}')
+print(f'pi ~= {math.pi:.75f}')
