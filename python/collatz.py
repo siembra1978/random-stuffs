d19a3a1e5826fa5bb7725dc59442c586198d071a
siembra <77788776+siembra1978@users.noreply.github.com>
2025-04-16 18:00:50 -0400

    h

--- python/collatz.py
+++ python/collatz.py
@@ -6,12 +6,13 @@ r = 1
 
 while True:
     n = r
-    i = r
     steps = 0
-    highest = 0
+    highest = r
+    streak = 0
+
     while n > 1:
         if n % 2 == 0:
-            n = n/2
+            n = n//2
         else:
             n = n*3 + 1
         
@@ -20,6 +21,6 @@ while True:
 
         steps += 1
 
-    time.sleep(.25)
-    print(f"Started with: {i}, Ended with {n}, Took {steps} steps, Highest was {highest}")
+    time.sleep(.125)
+    print(f"Started with: {r}, Took {steps} steps, Highest was {highest}")
     r += 1
