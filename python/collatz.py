e8576e954f7ffafbe5dc724773e833323c24276b
siembra <77788776+siembra1978@users.noreply.github.com>
2025-04-15 21:05:24 -0400

    Update collatz.py

--- python/collatz.py
+++ python/collatz.py
@@ -1,18 +1,25 @@
 import random
+import time
 
-r = 10000000000000000000000000000000000000000000000000000000000000
-#r = 100
+#r = 10000000000000000000000000000000000000000000000000000000000000
+r = 1
 
 while True:
     n = r
     i = r
     steps = 0
+    highest = 0
     while n > 1:
         if n % 2 == 0:
             n = n/2
         else:
             n = n*3 + 1
+        
+        if n > highest:
+            highest = n
+
         steps += 1
 
-    print(f"Started with: {i}, Ended with {n}, Took {steps} steps")
+    time.sleep(.25)
+    print(f"Started with: {i}, Ended with {n}, Took {steps} steps, Highest was {highest}")
     r += 1
