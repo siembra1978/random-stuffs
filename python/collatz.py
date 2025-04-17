a1e5beba3d28d642f26db270e9c19815c89dc700
siembra <77788776+siembra1978@users.noreply.github.com>
2025-04-17 13:20:40 -0400

    Update collatz.py

--- python/collatz.py
+++ python/collatz.py
@@ -1,18 +1,21 @@
 import random
+import time
 
-r = 10000000000000000000000000000000000000000000000000000000000000
-#r = 100
+#r = 10000000000000000000000000000000000000000000000000000000000000
+r = 1
 
 while True:
     n = r
     i = r
     steps = 0
     while n > 1:
+        print(n)
         if n % 2 == 0:
             n = n/2
         else:
             n = n*3 + 1
         steps += 1
+        time.sleep(.5)
 
     print(f"Started with: {i}, Ended with {n}, Took {steps} steps")
     r += 1
